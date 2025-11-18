f4a4e96f791c63c5a1476d881be5711622c0f423
Zolani Mthembu <zolanim@hollywoodbets.net>
2025-11-18 06:45:53 +0200

    finished question to and added example test

--- TechnicalTest/QuestionTwo.cs
+++ TechnicalTest/QuestionTwo.cs
@@ -8,9 +8,44 @@ namespace TechnicalTest
 {
     public class QuestionTwo
     {
-        //public static int GetPitDepth(int[] A)
-        //{
-        //    if (A == null || A.Length < 3) return -1;
-        //}
+        public static int GetPitDepth(int[] A)
+        {
+            if (A == null || A.Length < 3) return -1;
+
+            int n = A.Length;
+            int i = 0;
+            int maxDepth = -1;
+
+            while (i < n - 1)
+            {
+                while (i < n - 1 && A[i] <= A[i + 1]) i++;
+                int P = i;
+
+                while (i < n - 1 && A[i] > A[i + 1]) i++;
+                int Q = i;
+
+                if (P == Q || Q == n - 1)
+                {
+                    continue;
+                }
+
+                while (i < n - 1 && A[i] < A[i + 1]) i++;
+                int R = i;
+
+                if (P < Q && Q < R)
+                {
+                    if (A[P] > 0)
+                    {
+                        long leftDepth = (long)A[P] - (long)A[Q];
+                        long rightDepth = (long)A[R] - (long)A[Q];
+                        long depth = Math.Min(leftDepth, rightDepth);
+                        if (depth > maxDepth) maxDepth = (int)depth;
+                    }
+                }
+
+            }
+
+            return maxDepth;
+        }
     }
 }
---
+++ UnitTest/QuestionTwoTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechnicalTest;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class QuestionTwoTests
+    {
+        [TestMethod]
+        public void ExampleTest()
+        {
+            int[] a = { 0, 1, 3, -2, 0, 1, 0, -3, 2, 3 };
+            Assert.AreEqual(4, QuestionTwo.GetPitDepth(a));
+        }
+    }
+}
