3f7f4c96f1d1a1982ec9cac5819dcd548d1d70dc
Zolani Mthembu <zolanim@hollywoodbets.net>
2025-11-18 06:55:08 +0200

    added tests for question two

--- UnitTest/QuestionTwoTests.cs
+++ UnitTest/QuestionTwoTests.cs
@@ -16,5 +16,18 @@ namespace UnitTest
             int[] a = { 0, 1, 3, -2, 0, 1, 0, -3, 2, 3 };
             Assert.AreEqual(4, QuestionTwo.GetPitDepth(a));
         }
+        [TestMethod]
+        public void ProvidedExtraTest()
+        {
+            int[] a = { 1, 2, 3, 1, 2, 3, 1, 2, 3 };
+            Assert.AreEqual(2, QuestionTwo.GetPitDepth(a));
+        }
+
+        [TestMethod]
+        public void NoPits_ReturnsMinusOne()
+        {
+            int[] a = { 1, 2, 3, 4, 5 };
+            Assert.AreEqual(-1, QuestionTwo.GetPitDepth(a));
+        }
     }
 }
