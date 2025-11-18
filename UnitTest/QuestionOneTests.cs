f2cfbcafa39ed3bcb6ee17c8e55754b4b68b0896
Zolani Mthembu <zolanim@hollywoodbets.net>
2025-11-18 06:35:08 +0200

    unit test for questin on

--- UnitTest/QuestionOneTests.cs
+++ UnitTest/QuestionOneTests.cs
@@ -16,5 +16,18 @@ namespace UnitTest
             string s = "We test coders. Give us a try?";
             Assert.AreEqual(4, QuestionOne.GetLongestSentance(s));
         }
+        [TestMethod]
+        public void ProvidedExtraTest()
+        {
+            string s = "Forget CVs..Save time . x x";
+            Assert.AreEqual(2, QuestionOne.GetLongestSentance(s));
+        }
+
+        [TestMethod]
+        public void EmptyAndNoWords()
+        {
+            Assert.AreEqual(0, QuestionOne.GetLongestSentance(""));
+            Assert.AreEqual(0, QuestionOne.GetLongestSentance("...!!!"));
+        }
     }
 }
