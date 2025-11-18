9bdbf3a34692f2db30b96b490fe761f453e63438
Zolani Mthembu <zolanim@hollywoodbets.net>
2025-11-18 06:17:39 +0200

    question 1 done

--- TechnicalTest/QuestionOne.cs
+++ TechnicalTest/QuestionOne.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace TechnicalTest
@@ -16,6 +17,18 @@ namespace TechnicalTest
             }
 
             var sentences = s.Split(new[] { '.', '?', '!' }, StringSplitOptions.None);
+
+            int maxWords = 0;
+            var letterRegex = new Regex("[A-Za-z]");
+
+            foreach (var raw in sentences)
+            {
+                var tokens = raw.Split((char[])null, StringSplitOptions.RemoveEmptyEntries);
+                int words = tokens.Count(t => letterRegex.IsMatch(t));
+                if (words > maxWords) maxWords = words;
+            }
+
+            return maxWords;
         }
     }
 }
